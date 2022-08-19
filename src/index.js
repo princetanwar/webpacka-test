a8339dfbce53073b208b0049da5c6217317606df
prince tanwar <prince.tanwar@affixious.com>
2022-08-19 11:08:26 +0530

    remove dist folder and add support for html creation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import _ from 'lodash';
+import printMe from './print.js';
+
 import './style.css'
 import Icon from './myholiday_logo.svg';
 import data from './data.json'
 
 function component() {
 	const element = document.createElement('div');
-      
-	// Lodash, now imported by this script
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 	element.classList.add('hello');
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	
+	
+	
+	const btn = document.createElement('button');
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
+	
+	element.appendChild(btn);
+
+
+
+	// Lodash, now imported by this script
       
 
 	 // Add the image to our existing div.
