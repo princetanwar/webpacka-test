5cb0e7d0945791ce75b665845d8690500e1e722b
prince tanwar <prince.tanwar@affixious.com>
2022-08-19 12:39:03 +0530

    add source map

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,17 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     }),
   ],
+  devtool: 'inline-source-map',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, "dist"),
